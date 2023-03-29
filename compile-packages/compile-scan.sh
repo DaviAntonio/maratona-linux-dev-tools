9e01249b53081de7d5ab5107b38ff2875362800c
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2023-03-29 17:15:57 -0300

    Fix incompatibility between Packages.gz and VM script
    The generated Packages.gz file expected the files to be placed in the
    `packages` directory. This conflicted with the virtual machine external
    repository configuration, which places the files in the repository's
    root directory.
    
    The aforementioned problem has been dealt with, and such action required
    the substitution of the relative directory approach to a directory
    changing one.

--- compile-packages/compile-scan.sh
+++ compile-packages/compile-scan.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 scan() {
-	local packages_dir="$1"
+	local -r original_dir="$(pwd)"
+	local -r packages_dir="$1"
+
+	# go into directory
+	cd "$packages_dir" || exit
 
 	# Scan the packages
 	printf "\nRun dpkg-scanpackages\n"
-	dpkg-scanpackages "$packages_dir" | gzip > "${packages_dir}/Packages.gz"
+	dpkg-scanpackages . | gzip > "Packages.gz"
+
+	cd "$original_dir"
 
 	printf "\nDone\n"
 	return 0
